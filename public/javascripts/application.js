b34e7e20f056c361fe1458e118bbca034a081dc6
Johan Sveholm <jsveholm@gmail.com>
2011-02-16 09:49:59 +0900

    live search

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,6 +3,10 @@ $(function(){
   if($(".error").text() == "") {
     $(":text:visible:enabled:first").focus();
   }
+  $("form#articles_search input").keyup(function() {
+    $.get($("form#articles_search").attr("action"), $("form#articles_search").serialize(), null, "script");
+    return false;
+  });
 });
 
 function add_fields(link, association, content){
@@ -10,6 +14,8 @@ function add_fields(link, association, content){
   var regexp = new RegExp("new_"+association,"g")
   $(link).parent().before(content.replace(regexp,new_id));
 }
+
+
 
 
 
